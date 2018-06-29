f3967c81dba503dcaf25b2afbd396547e01855ed
Tim Cavanagh <tim.cavanagh@adelaide.edu.au>
2018-06-29 14:53:51 +0930

    ADX- 386 Initial commit of course events rewrite to content events

--- services/eventcount/service.py
+++ services/eventcount/service.py
@@ -1,21 +1,24 @@
 """
 Service for importing the edX clickstream
 """
+import warnings
 
-import json
-import time
-import urllib2
-from datetime import datetime
+import MySQLdb
+import dateutil
 
 import base_service
-import config
 import utils
-from pymongo import MongoClient
+import time
+import config
+import urllib2
+import json
+from datetime import datetime
 
 
 class Eventcount(base_service.BaseService):
     """
-    Collects the clickstream logs from the edX data package using mongoimport system command
+    Collects the student engagement data - chapter, sequential and video logs
+    from courseware_studentmodule table by week of activity
     """
     inst = None
 
@@ -36,14 +39,9 @@ class Eventcount(base_service.BaseService):
         self.ec_db = "Course_Event"
 
         self.sql_ec_conn = None
-        # self.sql_course_conn = None
+        self.sql_cm_conn = None
 
-        self.mongo_dbname = "logs"
-        self.mongo_collectionname = "clickstream"
-
-        self.mongo_client = None
-        self.mongo_db = None
-        self.mongo_collection = None
+        self.cm_table = "courseware_studentmodule"
 
         self.courses = {}
 
@@ -55,11 +53,9 @@ class Eventcount(base_service.BaseService):
         """
         Set initial variables before the run loop starts
         """
-        ensure_mongo_indexes()
         self.courses = self.get_all_courses()
         self.sql_ec_conn = self.connect_to_sql(self.sql_ec_conn, self.ec_db, True)
-        self.connect_to_mongo(self.mongo_dbname, self.mongo_collectionname)
-        # self.sql_course_conn = self.connect_to_sql(self.sql_course_conn, "", True)
+        self.sql_cm_conn = self.connect_to_sql(self.sql_cm_conn, "", True)
 
         print "SETTING UP EVENT COUNT"
         pass
@@ -68,21 +64,26 @@ class Eventcount(base_service.BaseService):
         """
         Runs every X seconds, the main run loop
         """
-        print "RUNNING THE EVENT COUNT"
-        utils.log("The Event Count starts at: " + str(datetime.now()))
-        self.clean_ec_db()
 
         last_run = self.find_last_run_ingest("EventCount")
         last_timefinder = self.find_last_run_ingest("TimeFinder")
+        last_dbstate = self.find_last_run_ingest("DatabaseState")
 
-        if self.finished_ingestion("TimeFinder") and last_run < last_timefinder:
+        if self.finished_ingestion("TimeFinder") \
+                and last_run < last_timefinder \
+                and self.finished_ingestion("DatabaseState") \
+                and last_run < last_dbstate:
+            print "RUNNING THE EVENT COUNT"
+            utils.log("The Event Count starts at: " + str(datetime.now()))
+            self.clean_ec_db()
             print "STARTING EVENT COUNT"
             for course_id, course in self.courses.items():
                 print "EVENT COUNTING " + str(course_id)
-                print course_id
+                # print course_id
 
                 # Get events from course info
                 json_file = course['dbname'].replace("_", "-") + ".json"
+                course_db_name = course['dbname']
                 courseinfo = self.loadcourseinfo(json_file)
                 if courseinfo is None:
                     utils.log("Can not find course info for ." + str(course_id))
@@ -93,15 +94,16 @@ class Eventcount(base_service.BaseService):
                 # Get events
                 events = self.get_events(courseinfo)
 
+                dates = self.get_dates(courseinfo)
+
                 # Create courseevent table
                 self.create_ec_table(course_id, events)
 
                 events_date_counts = {}
 
+                # get the date from courseware_studentmodule
                 for event_id in events:
-                    self.group_event_by_date(course['mongoname'], event_id, events_date_counts)
-
-                # print events_date_counts
+                    self.group_event_by_date(course_db_name, event_id, events_date_counts, dates)
 
                 # Insert records into database
                 self.insert_ec_table(course_id, events_date_counts)
@@ -110,30 +112,42 @@ class Eventcount(base_service.BaseService):
 
             self.loop = False
             utils.log("The Event Count ends at: " + str(datetime.now()))
+            self.save_run_ingest()
 
-    def group_event_by_date(self, course_mongo_name, event_id, events_date_counts):
+    def get_dates(self, courseinfo):
+        """
+        Get the course start and end dates from the courseinfo file
+        :param courseinfo:
+        :return:
+        """
+        course_launch_date = None
+        course_close_date = None
+        if 'start' in courseinfo:
+            try:
+                course_launch_time = dateutil.parser.parse(courseinfo['start'].replace('"', ""))
+                course_launch_date = course_launch_time.date()
+            except Exception:
+                print "ERROR: BAD COURSE START DATE"
+                pass
+        else:
+            utils.log("Course start date not found")
+            pass
+        if 'end' in courseinfo:
+            try:
+                course_close_time = dateutil.parser.parse(courseinfo['end'].replace('"', ""))
+                course_close_date = course_close_time.date()
+            except Exception:
+                print "ERROR: BAD COURSE END DATE"
+                pass
+        else:
+            utils.log("Course end date not found")
 
-        results = self.mongo_collection.find(
-                {"event_type": {"$regex": event_id}, "context.course_id": course_mongo_name})
-        for item in results:
-            if 'time_date' in item:
-                item_date = item['time_date'].date()
-            elif 'time' in item:
-                item_date = datetime.strptime(item['time'][:19], '%Y-%m-%dT%H:%M:%S').date()
-            else:
-                continue
-
-            if item_date not in events_date_counts:
-                events_date_counts[item_date] = {}
-                events_date_counts[item_date][event_id] = 0
-            elif event_id not in events_date_counts[item_date]:
-                events_date_counts[item_date][event_id] = 0
-
-            events_date_counts[item_date][event_id] += 1
+        return [course_launch_date, course_close_date]
 
     def get_chapters(self, obj, found=None):
         """
         Gets the chapter for the object (recursive)
+        :param self:
         :param obj: the object being added
         :param found: an array of previously found elements
         :return the found object
@@ -150,8 +164,10 @@ class Eventcount(base_service.BaseService):
     def get_events(self, courseinfo):
         """
         Analyses chapters within a course
+        :param courseinfo: The json object containing the course structure
+        :param self:
         :param chapters: An array of chapter dictionaries
-        :return: A dict containing the analysis
+        :return: A dict containing the events in order
         """
 
         chapters = []
@@ -159,13 +175,21 @@ class Eventcount(base_service.BaseService):
 
         events = []
         for chapter in chapters:
-            for sequential in chapter['children']:
-                if sequential['tag'] == 'sequential' and 'children' in sequential:
-                    for vertical in sequential['children']:
-                        if vertical['tag'] == 'vertical' and 'children' in vertical:
-                            for child in vertical['children']:
-                                if 'url_name' in child:
-                                    events.append(child['url_name'])
+            # check for event id and not invisible
+            if 'url_name' in chapter and 'visible_to_staff_only' not in chapter:
+                events.append(chapter['url_name'])
+                for sequential in chapter['children']:
+                    if sequential['tag'] == 'sequential' and 'children' in sequential:
+                        # check for event id and not invisible
+                        if 'url_name' in sequential and 'visible_to_staff_only' not in sequential:
+                            events.append(sequential['url_name'])
+                            for vertical in sequential['children']:
+                                if vertical['tag'] == 'vertical' and 'children' in vertical:
+                                    for child in vertical['children']:
+                                        # check for event id and not invisible
+                                        if 'url_name' in child and 'visible_to_staff_only' not in child:
+                                            if child['tag'] == 'video':
+                                                events.append(child['url_name'])
 
         return events
 
@@ -196,7 +220,11 @@ class Eventcount(base_service.BaseService):
         self.sql_ec_conn.commit()
 
     def insert_ec_table(self, course_id, events_date_counts):
-
+        """
+        Inserts the counts for the related events into event count table
+        :param course_id:
+        :param events_date_counts:
+        """
         cursor = self.sql_ec_conn.cursor()
 
         ec_tablename = self.ec_table + "_" + course_id
@@ -238,6 +266,7 @@ class Eventcount(base_service.BaseService):
 
     def clean_ec_db(self):
         cursor = self.sql_ec_conn.cursor()
+        warnings.filterwarnings('ignore', category=MySQLdb.Warning)
 
         for course_id, course in self.courses.items():
             ec_tablename = self.ec_table + "_" + course_id
@@ -245,40 +274,49 @@ class Eventcount(base_service.BaseService):
             cursor.execute(query)
 
         self.sql_ec_conn.commit()
+        warnings.filterwarnings('always', category=MySQLdb.Warning)
         utils.log(self.ec_db + " has been cleaned.")
 
-    def connect_to_mongo(self, db_name="", collection_name=""):
+    def group_event_by_date(self, course_db_name, event_id, events_date_counts, dates):
         """
-        Connects to a mongo database
-        :param db_name:
-        :param collection_name:
-        :return:
+        Gets the dates for each students interaction with given event
+        :param self:
+        :param course_db_name:
+        :param event_id:
+        :param events_date_counts:
         """
+        # print event_id
+        # print course_db_name
+        # check we have valid dates and assign them - else ignore them
+        date_filter = "1 = 1"
         try:
-            if self.mongo_client is None:
-                self.mongo_client = MongoClient('localhost', 27017)
-            if db_name != "":
-                self.mongo_db = self.mongo_client[db_name]
-                if self.mongo_db:
-                    self.mongo_dbname = db_name
-                    if collection_name != "":
-                        self.mongo_collection = self.mongo_db[collection_name]
-                        if self.mongo_collection:
-                            self.mongo_collectionname = collection_name
-            return True
-        except Exception, e:
-            utils.log("Could not connect to MongoDB: %s" % e)
-        return False
+            start_date = dates[0].strftime("%Y-%m-%d")
+            end_date = dates[1].strftime("%Y-%m-%d")
+            date_filter = "date(`created`) between  '" + start_date + "' AND '" + end_date + "' "
+        except ValueError:
+            pass
 
+        self.sql_cm_conn = self.connect_to_sql(self.sql_cm_conn, course_db_name, True)
+        query = "select date(`created`) time_date from `courseware_studentmodule` where `module_id` like '%" + event_id + "' and " + date_filter + " "
+        cursor = self.sql_cm_conn.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(query)
+        results = cursor.fetchall()
+        cursor.close()
 
-def ensure_mongo_indexes():
-    """
-    Runs commands on the mongo indexes to ensure that they are set
-    :return: None
-    """
-    utils.log("Setting index for countries")
-    cmd = config.MONGO_PATH + "mongo  --quiet " + config.MONGO_HOST + "/logs --eval \"db.clickstream.ensureIndex({country:1})\""
-    # os.system(cmd)
+        for item in results:
+            if 'time_date' in item:
+                item_date = item['time_date']
+            else:
+                continue
+
+            if item_date not in events_date_counts:
+                events_date_counts[item_date] = {}
+                events_date_counts[item_date][event_id] = 0
+            elif event_id not in events_date_counts[item_date]:
+                events_date_counts[item_date][event_id] = 0
+
+            events_date_counts[item_date][event_id] += 1
+        pass
 
 
 def get_files(path):
